4766ea5320611199e43a8f706cf57fc5e31aa398
Maksym Kiporenko <kiporenkomaks8@gmail.com>
2025-06-26 22:58:16 +0300

    Decided

--- app/main.py
+++ app/main.py
@@ -1,13 +1,34 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed"
+    } if len(errors) > 0 else {
+        "path": file_path,
+        "status": "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+
+    return [
+        {
+            "errors": [],
+            "path": linter,
+            "status": "passed"
+        } if len(linter_report[linter]) == 0 else
+        format_single_linter_file(linter, linter_report[linter])
+        for linter in linter_report
+    ]
